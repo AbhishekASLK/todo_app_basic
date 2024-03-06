75d5e587ae93960f219af9deed8d75c71b2bcccf
AbhishekASLK <bhoslea925@gmail.com>
2024-03-06 22:00:51 +0530

    Initialized the todo-app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_basic/screens/login.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
